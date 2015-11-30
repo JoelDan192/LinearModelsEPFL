35e5861d4bbd0757e7de511c48d8fc57e45d4591
matteo Murgia <matteo@tsf-476-wpa-4-166.epfl.ch>
2015-11-30 11:37:15 +0100

    initiate the second part of the practical

--- codePractical2.R
+++ codePractical2.R
@@ -16,3 +16,10 @@ fit2 = lm(hundredOverMPG ~ HPOverWeight + weights)
 # Creation des tables d'ANOVA
 anova(fit1)
 anova(fit2)
+
+# ------------------- PART 2 : Model Selection --------------------
+
+# On veut maintenant utiliser les variables de 11 à 26 pour créer notre model
+newData <- cars[ ,11:26]
+
+newFit <- lm(y ~ ., data = newData)

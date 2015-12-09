3de440c52622ecb78f4c794455aacd2ca762f1c0
matteo Murgia <matteo@MacBook-Pro-de-matteo.local>
2015-12-09 19:39:03 +0100

    part 2 until d) is done, but I'm not really sure If the models I found are correct. We juste have to check, and choose one and do Diagnostic part

--- codePractical2.R
+++ codePractical2.R
@@ -62,9 +62,51 @@ summary(f4.forward)
 vif(f4.forward)
 
 
+# --------------------------------------------------------------------------------------
 # -------------- Model sans les prédicteurs montrant un signe de multicollinéarité -----
-newData2 = cars[,-c(1,2,3,4,5,6,7,8,9,10,17,19,20,21,22)]
 
+# Scatterplots des variables fortement corrélées
+pairs(~Weight+FuelCapacity+Length+Wheelbase+Width+Uturn, data=cars)
+
+# Nouvelle régression sans ces variables
+newData2 = cars[,-c(1,2,3,4,5,6,7,8,9,10,17,19,20,21,22)]
+newFit <- lm(hundredOverMPG ~ ., data = newData2)
+summary(newFit)
+
+# Latex table
+summaryNewFit <- xtable(summary(newFit))
+print(summaryNewFit)
+
+# VIF of the full model
+library(car)
+vif(newFit)
+
+
+# ------ Model Construction : Backward and Forward selection using AIC and BIC
+
+# AIC, Backward
+f1 <- lm(hundredOverMPG ~ ., data = newData2)
+f1.backward <- step(f1, direction = "backward")
+summary(f1.backward)
+vif(f1.backward)
+# AIC, Forward
+f2 <- lm(hundredOverMPG ~ 1, data = newData2)
+my.scope <- formula(newData2)
+f2.forward <- step(f2, scope = my.scope, direction = "forward", data = newData2)
+summary(f2.forward)
+vif(f2.forward)
+
+# BIC, Backward
+f3 <- lm(hundredOverMPG ~ ., data = newData2) # Fitting model using all the covariates
+f3.backward <- step(f3, direction = "backward", k=log(length(newData2)))
+summary(f3.backward)
+vif(f3.backward)
+# BIC, Forward
+f4 <- lm(hundredOverMPG ~ 1, data = newData2) # Fitting the model with only one varialbe, the 1 column
+my.scope <- formula(newData2)
+f4.forward <- step(f4, scope = my.scope, direction = "forward", data = newData2, k=log(length(newData2)))
+summary(f4.forward)
+vif(f4.forward)
 
 
 

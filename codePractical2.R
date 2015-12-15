a71737133777d3f3a3cda59196231058a0936186
Joel Castellon <joel.castellonarevalo@epfl.ch>
2015-12-15 06:37:49 +0100

    Report for second practical Linear Models. ANOVA, Forward/Backward model selection, AIC/BIC criteria.

--- codePractical2.R
+++ codePractical2.R
@@ -1,5 +1,7 @@
 load('cars.RData')
 #install.packages("xtable");
+#?install.packages('car')
+require(car) 
 library(xtable)
 
 # the response y : 100 / City MPG
@@ -7,17 +9,42 @@ hundredOverMPG <- rep(100,82) / cars$CityMPG
 
 # the variables
 weights <- cars$Weight
-
 HPOverWeight <- cars$Horsepower / cars$Weight
 
-# premier model dans l ordre Y = b0 + X1*b1 + X2*b2
-fit1 = lm(hundredOverMPG ~ weights + HPOverWeight)
-# deuxième model model dans l ordre Y = b0 + X2*b2 + X1*b1
-fit2 = lm(hundredOverMPG ~ HPOverWeight + weights)
+# Look if there is a correlation between our y and the variable we are going to use
+#pdf(file="ScatterWeights.pdf", width=6,height=4)
+#plot(weights, hundredOverMPG, xlab='Weights', ylab='Gallons per hundred city miles',pch='*')
+#dev.off()
 
-# Creation des tables d'ANOVA
+#pdf(file="ScatterPower.pdf", width=6,height=4)
+#plot(HPOverWeight, hundredOverMPG, xlab='HorsePowerPerWeight', ylab='Gallons per hundred city miles',pch='*')
+#dev.off()
+
+# correlations
+cor(weights, hundredOverMPG)#0.9007547
+cor(HPOverWeight, hundredOverMPG)#0.5976514
+
+# Fit first model in order: Y = b0 + X1*b1 + X2*b2
+fit1 = lm(hundredOverMPG ~ weights + HPOverWeight)
+fit1_w = lm(hundredOverMPG ~ weights)
+# Fit second model in order: Y = b0 + X2*b2 + X1*b1
+fit2 = lm(hundredOverMPG ~ HPOverWeight + weights)
+fit2_hp = lm(hundredOverMPG ~ HPOverWeight)
+# ANOVA tables
 anova(fit1)
+anova(fit1_w, fit1)
 anova(fit2)
+anova(fit2_hp, fit2)
+#pairs(~FuelCapacity+Length+Wheelbase+Width+Uturn, data=cars, pch='*')
+
+print(xtable(anova(fit1)))
+print(xtable(anova(fit2)))
+
+cor(cars$Weight,cars$FuelCapacity)#0.9001302
+cor(cars$FuelCapacity,cars$Wheelbase)#0.7934098
+cor(cars$FuelCapacity,cars$Uturn)#0.6741421
+cor(cars$FuelCapacity,cars$Width)#0.7717577
+cor(cars$FuelCapacity,cars$Length)#0.794308
 
 # ------------------- PART 2 : Model Selection --------------------
 
@@ -28,14 +55,14 @@ FullFit <- lm(hundredOverMPG ~ ., data = newData)
 summary(FullFit)
 
 # Latex table
-summaryFullFit <- xtable(summary(FullFit))
-print(summaryFullFit)
+#summaryFullFit <- xtable(summary(FullFit))
+#print(summaryFullFit)
 
 # VIF of the full model
 library(car)
 vif(FullFit)
 
-# ------ Model Construction : Backward and Forward selection using AIC and BIC
+# ---Model Construction : Backward/Forward selection using AIC/BIC
 
 # AIC, Backward
 f1 <- lm(hundredOverMPG ~ ., data = newData)
@@ -62,10 +89,10 @@ summary(f4.forward)
 vif(f4.forward)
 
 
-# --------------------------------------------------------------------------------------
-# -------------- Model sans les prédicteurs montrant un signe de multicollinéarité -----
+# --------------------------------------------------------------------
+# ----Model without predictors showing a sign of multicollinearity----
 
-# Scatterplots des variables fortement corrélées
+# Scatterplots of variables that are strongly correlated
 pairs(~Weight+FuelCapacity+Length+Wheelbase+Width+Uturn, data=cars)
 
 # Nouvelle régression sans ces variables
@@ -74,8 +101,8 @@ newFit <- lm(hundredOverMPG ~ ., data = newData2)
 summary(newFit)
 
 # Latex table
-summaryNewFit <- xtable(summary(newFit))
-print(summaryNewFit)
+#summaryNewFit <- xtable(summary(newFit))
+#print(summaryNewFit)
 
 # VIF of the full model
 library(car)
@@ -108,40 +135,39 @@ f4.forward <- step(f4, scope = my.scope, direction = "forward", data = newData2,
 summary(f4.forward)
 vif(f4.forward)
 
-# We see that we have two models, one with the BIC ( backward anf forward give the same model)
+# We see that we have two models, one with the BIC ( backward and forward give the same model)
 # and one with the AIC
 
 # ---------------------------- DIAGNOSTICS FOR THE TWO MODELS ------------------------------
-
-modelA <- f2.forward
 modelB <- f4.forward
 
 # Confidence interval at 95%
-confint(modelA,level=0.95)
 confint(modelB,level=0.95)
 
 # Plot fitted values against standardised residual
-plot(modelA$fitted.values, rstandard(modelA), xlab="Fitted Values", ylab="Standardised residuals")
-plot(modelB$fitted.values, rstandard(modelB), xlab="Fitted Values", ylab="Standardised residuals")
+pdf(file="fitted.pdf", width=6,height=4)
+plot(modelB$fitted.values, rstandard(modelB), xlab="Fitted Values"
+     , ylab="Standardised residuals", pch='*')
+dev.off()
 
 # QQ plot
-qqnorm(rstandard(modelA))
-qqline(rstandard(modelA))
-
-qqnorm(rstandard(modelB))
+pdf(file="qq.pdf", width=6,height=4)
+qqnorm(rstandard(modelB),pch='*')
 qqline(rstandard(modelB))
+dev.off()
 
 # Cook Distance
-plot(cooks.distance(modelA), xlab='Observations', ylab='Cook distance', main='Cook Distance Plot', ylim=c(0,0.2))
-p <- dim(model.matrix(modelA))[2]
-n <- dim(model.matrix(modelA))[1]
-abline(8/(n-2*p),0)
-plot(hatvalues(modelA))
-abline(2*p/n,0)
 
-plot(cooks.distance(modelB), xlab='Observations', ylab='Cook distance', main='Cook Distance Plot', ylim=c(0,0.2))
+pdf(file="cook.pdf", width=6,height=4)
+plot(cooks.distance(modelB), xlab='Observations', ylab="Cook's distance",
+     ylim=c(0,0.2), pch='*')
 p <- dim(model.matrix(modelB))[2]
 n <- dim(model.matrix(modelB))[1]
 abline(8/(n-2*p),0)
-plot(hatvalues(modelB))
+dev.off()
+
+pdf(file="hats.pdf", width=6,height=4)
+plot(hatvalues(modelB), xlab = "Index", ylab="Hat-values",pch="*")
 abline(2*p/n,0)
+dev.off()
+
